1b3ec957b9d54223d5cba76f935eebb23c29f4a4
abdullox <abdulloxislomov39@gmail.com>
2024-10-11 01:26:45 +0500

    comment

--- config/settings.py
+++ config/settings.py
@@ -57,12 +57,8 @@ SIMPLE_JWT = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',  # Yoki JWT bo'lsa 'rest_framework_simplejwt.authentication.JWTAuthentication'
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
+
 }
 
 MIDDLEWARE = [
